3f48edc34477f909ec4699b4326029891e4ffed7
Thomas P. Harte <tharte@cantab.net>
2017-06-30 18:43:25 -0400

    add package help: tutils.dev-package.R

---
+++ R/tutils.dev-package.R
@@ -0,0 +1,21 @@
+# Copyright Thomas P. Harte 2017
+
+#' tutils.dev: Thomas's functions to make life easy (or easier)
+#'
+#' This package contains some functions that the author has
+#' developed over the years and now can't do without.
+#' Functions such as \code{\link{whos}} are essential to remain sane.
+#'
+#' @author Thomas P. Harte \cr \url{https://github.com/tharte} \cr \cr
+#'
+#' Thomas P. Harte \email{tharte@@cantab.net} \cr
+#'
+#' @references \url{https://github.com/tharte/tutils}
+#'
+#' @keywords package
+#'
+#' @docType package
+#'
+#' @name tutils.dev
+#' @rdname tutils.dev_package
+NULL
