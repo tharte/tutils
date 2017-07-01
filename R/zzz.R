f0dcfed68bb4e6678e3d90665658322f9ab279b2
Thomas P. Harte <tharte@cantab.net>
2017-07-01 14:25:24 -0400

    up-rev zzz.R [.on{Load,Unload}]

--- R/zzz.R
+++ R/zzz.R
@@ -1,27 +1,25 @@
-.First.lib<- function(libname, pkgname) {
-# TODO: This mechanism for loading dependent libraries has been 
-#       almost entirely superseded by using the 'Depends:' field in the 
+.onLoad<- function(libname, pkgname) {
+# FIXME: This mechanism for loading dependent libraries has been
+#       almost entirely superseded by using the 'Depends:' field in the
 #       'DESCRIPTION' file of a package.
-	require(zoo, quietly=FALSE)
-	
+#	require(zoo, quietly=FALSE)
+
 
 	switch(Sys.info()["sysname"],
-		Windows = { 
-			require(xlsReadWrite, quietly=FALSE) 
+		Windows = {
+			require(xlsReadWrite, quietly=FALSE)
 		},
-		Linux = { 
+		Linux = {
 			# the (default) type="cairo" for x11 is an antialiasing
-			# graphics engine that is woefully slow on Ubuntu 9.04; type="Xlib" 
+			# graphics engine that is woefully slow on Ubuntu 9.04; type="Xlib"
 			# produces horridly retro bitmapped graphics; type="nbcairo" is good:
 			`windows`<- function(width=7, height=7) x11("", width, height, type="nbcairo")
 			assign("windows", windows, .GlobalEnv)
 		}
 	)
 
-	# library.dynam("tutils", pkgname, libname)
-
 	# unfurl the start-up banner
-	str<- read.dcf(file.path(libname, pkgname, "DESCRIPTION"), 
+	str<- read.dcf(file.path(libname, pkgname, "DESCRIPTION"),
 			fields=c("Package", "Version", "Description"))
 	package.name<-        str[,"Package"]
 	package.version<-     str[,"Version"]
@@ -30,10 +28,9 @@
 	cat(package.description, "\n")
 }
 
-.Last.lib<- function(libpath) {
+.onUnload<- function(libpath) {
 	# library.dynam.unload("Commodities", libpath)
 	# print out some random message:
 	# `windows`<- function(width, height) x11("", width, height)
 	# 	cat("\ngoodbye, cruel world...\n")
 }
-
