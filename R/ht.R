c8fc446c1eef6b8c52b520c6ac51bfa7dffcc2b2
Thomas P. Harte <tharte@cantab.net>
2017-06-30 20:58:11 -0400

    add roxygen2 help to ht

--- R/ht.R
+++ R/ht.R
@@ -1,5 +1,33 @@
-`ht`<- 
-function (x, n = 6, ...) {
+#' Returns the head and tail part of an object
+#'
+#' \code{\link{sort.df}} sorts the columns of a \code{\link{data.frame}}
+#'
+#' @param x an object.
+#' @param n \code{\link{integer}}
+#'       If positive, size for the resulting object:
+#'          number of elements for a vector (including lists), rows for a
+#'          matrix or data frame or lines for a function. If negative,
+#'          all but the 'n' last/first number of elements of 'x'
+#' @param ... arguments to \code{\link{head}} and \code{\link{tail}}
+#'
+#' @return \code{\link{character}}
+#'
+#' @author Thomas P. Harte
+#'
+#' @keywords \code{\link{sort}}
+#'
+#' @seealso \code{\link{head}}, \code{\link{tail}}
+#'
+#' @examples
+#'	 N<- 20
+#'	 df<- data.frame(Name=letters[1:N], Value=1:N)
+#'	 ht(df)
+#'	 N<- 3
+#'	 df<- data.frame(Name=letters[1:N], Value=1:N)
+#'	 ht(df)
+#'
+#' @export
+`ht`<- function (x, n = 6, ...) {
 	if (any("list" %in% class(x))) {
 		if (is.null(names(x)))
 			    nm <- paste("[[", 1:length(x), "]]", sep = "")
@@ -28,4 +56,3 @@ function (x, n = 6, ...) {
 		}
 	}
 }
-
