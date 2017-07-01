2c7e514d822f0f55de9e2f1c689a039985625c8c
Thomas P. Harte <tharte@cantab.net>
2017-06-30 20:40:08 -0400

    add roxygen2 help to getExtension

--- R/getExtension.R
+++ R/getExtension.R
@@ -1,5 +1,33 @@
-`getExtension`<- 
-function (x, 
+#' Get file extension
+#'
+#' Returns the file extension of a file name
+#'
+#' @param x \code{\link{character}} a file name
+#' @param parse \code{\link{logical}} if TRUE, the filename
+#'   is parsed and returned as a \code{\link{list}}
+#'	 with two elements: basename and extension
+#'
+#' @return \code{\link{character}} file extension
+#'
+#' @author Thomas P. Harte
+#'
+#' @keywords \code{\link{sort}}
+#'
+#' @seealso \code{\link[limma]{removeExt}}
+#'
+#' @examples
+#'   # "csv" :
+#'   getExtension("foo.bar.csv")
+#'   # "csv" :
+#'   getExtension("foo.bar.csv", parse=TRUE)
+#'   # $basename
+#'   # [1] "foo.bar"
+#'   # $extension
+#'   # [1] "csv"
+#'
+#' @export
+`getExtension`<- function (
+    x,
 	parse=FALSE	# parse the string into basename & file extension
 ) {
 	stopifnot(is.character(x))
@@ -7,11 +35,10 @@ function (x,
 		if (parse)
 			return(list(basename=x, extension=character(0)))
 		else 	return("")
-	# cf. limma::removeExt 
+	# cf. limma::removeExt
 	extension<- sub("(.*)\\.(.*)$", "\\2", x)
 	basename<- sub("(.*)\\.(.*)$", "\\1", x)
 	if (parse)
 		return(list(basename=basename, extension=extension))
 	else return(extension)
 }
-
