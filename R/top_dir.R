a3f1c8e1c1f5c8c54a879d69b04df694172f3aa0
Thomas P. Harte <tharte@cantab.net>
2017-07-02 14:46:31 -0400

    rename top.dir -> top_dir; mv top.dir.R -> top_dir.R

--- R/top_dir.R
+++ R/top_dir.R
@@ -2,7 +2,7 @@
 #'
 #' You can explicitly set the directory by providing the argument
 #' \code{.top.dir} with a path, or you can call the function
-#' without an argument and \code{top.dir} will make an operating-system
+#' without an argument and \code{top_dir} will make an operating-system
 #' specific guess as to the appropriate path.
 #'
 #' This is necessary for the \code{sweave} script's operation
@@ -11,10 +11,10 @@
 #'
 #' \code{.top.dir} is usually \code{~/tc} on Linux and \code{c:/tc} on Windoze.
 #'
-#' Regardless, once \code{top.dir} is called it assigns
+#' Regardless, once \code{top_dir} is called it assigns
 #' the chosen path to the variable \code{.top.dir} in \code{.GlobalEnv}
 #'
-#' Making a call to \code{top.dir} with no arguments will return
+#' Making a call to \code{top_dir} with no arguments will return
 #' the value of the variable \code{.top.dir} that has been assigned
 #' in \code{.GlobalEnv}, i.e. it acts an enquiry only.
 #'
@@ -22,25 +22,25 @@
 #'
 #' 	\code{\link{.top.dir}}
 #'
-#'	Why does \code{top.dir} assign the variable .top.dir to
+#'	Why does \code{top_dir} assign the variable .top.dir to
 #'	\code{.GlobalEnv}?
-#'	This is simply because 	\code{top.dir} takes an argument
+#'	This is simply because 	\code{top_dir} takes an argument
 #'	\code{.top.dir} which can be different from the default.
 #'	Typically in a project the top-level directory is defined once,
 #'	and this may be different from the default, say "c:/" on Windows
-#'	instead of "c:/tc". Instead of calling \code{top.dir} as
-#'	\code{top.dir(.top.dir="c:")}  this call can be made once at
+#'	instead of "c:/tc". Instead of calling \code{top_dir} as
+#'	\code{top_dir(.top.dir="c:")}  this call can be made once at
 #'	the start of the code block and subsequent references to it
 #'	can be made via the variable \code{.top.dir} instead of
 #'	calling the function explicitly each time as
-#'	\code{top.dir(.top.dir="c:")}. This isolates the change in
+#'	\code{top_dir(.top.dir="c:")}. This isolates the change in
 #'	the top-level directory to the variable \code{.top.dir}.
 #'
 #'	Thus the calling convention is to establish the top-level
-#'	directory with a call to \code{top.dir} either explicitly
+#'	directory with a call to \code{top_dir} either explicitly
 #'	naming the top-level directory, e.g.
-#'	\code{top.dir(.top.dir="c:")} or relying on the default
-#'	for the system using \code{top.dir()}. After this all
+#'	\code{top_dir(.top.dir="c:")} or relying on the default
+#'	for the system using \code{top_dir()}. After this all
 #'	references to the top-level directory can be made through
 #'	the variable \code{.top.dir}.
 #'
@@ -50,23 +50,23 @@
 #' 	# set the top-level directory to the system default
 #' 	# (every project should start this way, the value .top.dir
 #' 	# is assigned in .GlobalEnv
-#' 	`check.top.dir`<- function() {
+#' 	`check_top_dir`<- function() {
 #' 		print( whos(sort="Name", env=.GlobalEnv) )
 #' 		if (exists(".top.dir", env=.GlobalEnv)) {
 #'			get(".top.dir", env=.GlobalEnv)
 #'		}
 #'		else {
 #'			# guess the top-level directory depending on the OS:
-#'			top.dir()
+#'			top_dir()
 #'			get(".top.dir", env=.GlobalEnv)
 #'		}
 #'	}
-#'	check.top.dir()
+#'	check_top_dir()
 #'
 #' @keywords \code{\link{Sys.info}}
 #'
 #' @export
-`top.dir`<- function(.top.dir=NULL) {
+`top_dir`<- function(.top.dir=NULL) {
 	# unless an explicit .top.dir is specified in the argument list,
 	# take the one in .GlobalEnv if it exists:
 	if (is.null(.top.dir) & exists(".top.dir", env=.GlobalEnv)) {
