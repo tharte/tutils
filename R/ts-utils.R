bfd879588483a8a7f53f7ae25b4e720797d49f80
Thomas P. Harte <tharte@cantab.net>
2017-07-04 14:28:21 -0400

    add function is_yearmon

--- R/ts-utils.R
+++ R/ts-utils.R
@@ -26,3 +26,33 @@
 	# paste(class(x), collapse=", ")=="Date"
 	inherits(x, "Date")
 }
+
+
+#' Check if the object is of class \code{\link{yearmon}}
+#'
+#' Check if the object is of class \code{\link{yearmon}}
+#'
+#' @param  x  object
+#'
+#' @return \code{\link{logical}}
+#'
+#' @author Thomas P. Harte
+#'
+#' @keywords \code{\link{yearmon}}
+#'
+#' @seealso \code{\link{yearmon}}
+#'
+#' @examples
+#'   require(zoo)
+#'   is_yearmon(as.yearmon("2017-07-04"))
+#'   is_yearmon("2017-07-04")
+#'
+#'   x<- as.yearmon(seq(as.Date("2017-07-01"), len=4, by=1))
+#'   is_yearmon(x)
+#'   is_yearmon(as.character(x))
+#'
+#'
+#' @export
+`is_yearmon`<- function(x) {
+    inherits(x, "yearmon")
+}
--- tests/testthat/test-ts-utils.R
+++ tests/testthat/test-ts-utils.R
@@ -11,3 +11,14 @@ test_that("'is_Date' works", {
     expect_false(is_Date(as.character(x)))
 
 })
+
+
+test_that("'is_yearmon' works", {
+
+    expect_true(is_yearmon(as.yearmon("2017-07-04")))
+    expect_false(is_yearmon("2017-07-04"))
+
+    x<- as.yearmon(seq(as.Date("2017-07-01"), len=4, by=1))
+    expect_true(is_yearmon(x))
+    expect_false(is_yearmon(as.character(x))
+})
