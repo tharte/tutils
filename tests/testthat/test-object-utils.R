08eb56556f02f6531f0ccb9f3b59891c39a0cf59
Thomas P. Harte <tharte@cantab.net>
2017-07-03 20:19:09 -0400

    test copy

--- tests/testthat/test-object-utils.R
+++ tests/testthat/test-object-utils.R
@@ -21,45 +21,45 @@ test_that("'first','last' work", {
 
 test_that("'copy' works", {
 
-#'  # NULL list:
-#' 	copy(list(), integer(0))
-#'
-#' 	# unnamed list:
-#'  x<- list(1, 2, 3)
-#'  x
-#'
-#'  copy(x, integer(0))
-#'  copy(x, 1:2)
-#'
-#'  # named list:
-#'  x<- list(a=1, b=2, c=3)
-#'  x
-#'  # integer index still works:
-#'  copy(x, 1:2)
-#'  # named index works too:
-#'  copy(x, c("a","b"))
-#'
-#'  ## FIXME: index mismatch
-#'  ## copy(x, 1:4)
-#'  ## copy(x, c("a","b"))
-#'  ## copy(x, c("a","b","d"))
-#'
-N<- 20
-df<- data.frame(Name=letters[1:N], Value=1:N)
-ht(df)
+    # NULL list:
+    expect_equal(
+        copy(list(), integer(0)),
+        list()
+    )
 
-N<- 3
-df<- data.frame(Name=letters[1:N], Value=1:N)
-ht(df)
+    # unnamed list:
+    x<- list(1, 2, 3)
 
     expect_equal(
-          sapply(strsplit(str, " "), first),
-          c("hello", "how")
+        copy(x, integer(0)),
+        list()
     )
 
     expect_equal(
-          sapply(strsplit(str, " "), last),
-          c("there", "you?")
+        copy(x, 1:2),
+        list(1, 2)
     )
+
+    # named list:
+    x<- list(a=1, b=2, c=3)
+
+    # integer index still works:
+    expect_equal(
+        copy(x, 1:2),
+        list(a=1, b=2)
+    )
+
+    # named index works too:
+    expect_equal(
+        copy(x, c("a","b")),
+        list(a=1, b=2)
+    )
+
+    if (0) {
+    ## FIXME: index mismatch
+    ## copy(x, 1:4)
+    ## copy(x, c("a","b"))
+    ## copy(x, c("a","b","d"))
+    }
 
 })
